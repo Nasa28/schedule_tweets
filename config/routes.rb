9a734d05ba6d59efee1a000b1e1a050462091728
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-02 13:12:26 +0100

    set up routes for main and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'about', to: 'about#index'
+  root to: 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
