d8d3228ba25f3fe08fc366f72ed5599af5316fa9
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-03 13:24:05 +0100

    set up error messages for failed form validation

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,4 +2,17 @@ class RegistrationController < ApplicationController
   def new 
     @user = User.new
   end
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Account created Successfully"
+    else
+      render :new
+      
+    end
+  end
+  private 
+  def user_params 
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
