79c62d1876277c5572a03c93f5af874a5f86c4d9
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-03 11:00:13 +0100

    add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "Not a valid email" }
+  
   has_secure_password
 end
