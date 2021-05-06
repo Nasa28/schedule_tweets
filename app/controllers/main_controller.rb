8b148cf58a883a4b9478b875488b4b5207c806f4
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-07 00:41:48 +0100

    add login session

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
   def index 
-    flash.now[:notice] = "Logged in succesfully"
-    flash.now[:alert] = "Invalid Email or password"
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,6 +5,7 @@ class RegistrationController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Account created Successfully"
     else
       render :new
