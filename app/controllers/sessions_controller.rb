52349ddae8ed8b69b8e6d7bd4c97eb67a4d53d78
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-07 22:53:29 +0100

    add login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,21 @@
 class SessionsController < ApplicationController
+  def new 
 
+  end
+
+  def create 
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(:password)
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      flash.now[:alert] = 'Invalid Email or password'
+      render :new
+    end
+  end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out succesfully'
+    redirect_to root_path, alert: 'Logged out succesfully'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'registration#new'
   post 'sign_up', to: 'registration#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   root to: 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
