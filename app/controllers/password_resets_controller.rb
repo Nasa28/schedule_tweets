bda72a8e6f39554734e29801a66a41e17bd2b842
Nasa28 <kaluchinasaagu@gmail.com>
2021-05-09 12:48:53 +0100

    add edit action to password_reset feature

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,8 +11,23 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit 
-    @user = User.find_signed!(params[:token], purpose: "Password reset")
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     redirect_to sign_in_path, alert: "Your token has expired, try again"
+  end
+
+  def update 
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was updated Successfully"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params 
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create 
     user = User.find_by(email: params[:email])
-    if user.present? && user.authenticate(:password)
+    if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully'
     else
